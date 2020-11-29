fb0a94cd384086bb10d9b7534df9cf5f327ea12c
Goobert <qdyd65@gmail.com>
2020-11-29 18:06:23 +0200

    This is the App factory for the API

---
+++ ccapp/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config,)
+    
+    from . import rates
+    app.register_blueprint(rates.bp)
+    # app.add_url_rule('/', endpoint='index')
+
+    @app.route('/')
+    def index():
+        return ('Index Page')
+    return app
