01707cda87a6c02586b080195ad0bc782bcee38e
Goobert <qdyd65@gmail.com>
2020-11-29 18:15:58 +0200

    Configured 404 error handling to the init.py

--- ccapp/__init__.py
+++ ccapp/__init__.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
+
 
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY='5b91c8351e9f93b949629a34',
     )
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
@@ -20,4 +21,10 @@ def create_app(test_config=None):
     @app.route('/')
     def index():
         return ('Index Page')
+
+    @app.errorhandler(404)
+    def resource_not_found(e):
+        return jsonify(error=str(e)), 404
+
+
     return app
